88ec2fae89a88ed4ed7b000ad5eb05d51928bd74
“Sanket <sanket.raut0912@gmail.com>
2024-02-04 23:07:44 +0530

    Resolved Parh - problem solve

--- Question4.js
+++ Question4.js
@@ -1,2 +1,17 @@
 // Problem 4: Resolve Path
 // Problem Statement: Create a function resolvePath(relativePath) that takes a relative path as input and resolves it to an absolute path using the path module. The function should print the resolved path to the console.
+
+const path = require('path');
+
+
+function resolvePath(relativePath){
+    const absolutePath = path.resolve(__dirname, relativePath);
+    console.log(`Resolved Path: ${absolutePath}`);
+}
+
+//Test Cases:
+resolvePath('../project/folder/file.txt');
+// Expected Output: Resolved Path: /Users/username/project/folder/file.txt
+
+resolvePath('nonexistent-folder/file.txt');
+// Expected Output: Resolved Path: /Users/username/nonexistent-folder/file.txt
