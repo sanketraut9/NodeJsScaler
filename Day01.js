87e6dd6cc7a53443abc9aaefbedc3bb540ade6d6
“Sanket <sanket.raut0912@gmail.com>
2024-02-05 00:34:02 +0530

    the global object

--- Day01.js
+++ Day01.js
@@ -1,1 +1,18 @@
 console.log("Hello World!");
+
+
+function sayHello(){
+    console.log('Hello from Sanket');
+}
+
+// sayHello()
+
+// window object does not present in nodejs. nodeJs uses global object
+
+// console.log(global);               //Js we use window object
+
+let name = 'sanket'
+console.log(name.global);                //undefine
+
+
+//nodeJs always keeps veribles , functions in this file scopes only.
