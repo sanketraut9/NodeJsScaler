1270a3ae2a94f0befbf8488a2bc655c7470d9e44
“Sanket <sanket.raut0912@gmail.com>
2024-02-05 00:56:52 +0530

    Modules and Modularity

---
+++ modulesAndModularity.js
@@ -0,0 +1,14 @@
+// *****************Modules and Modularity******************//
+// Any file itself is a Module
+
+let name = 'Sanket'
+
+
+
+const  calculator = require('./calculator');
+
+
+calculator.addition(22,11)
+calculator.substraction(22,11)
+calculator.multiplication(22,11)
+calculator.division(22,11)
