348a636bf05b9f6267f4d515a353de4acad2d90d
“Sanket <sanket.raut0912@gmail.com>
2024-02-05 21:05:18 +0530

    figlet package

--- NodePackegeManager/figlet.js
+++ NodePackegeManager/figlet.js
@@ -1,6 +1,6 @@
 const figlet = require("figlet");
 
-figlet("Hello World!!", function (err, data) {
+figlet("Hello Sanket!!", function (err, data) {
   if (err) {
     console.log("Something went wrong...");
     console.dir(err);
