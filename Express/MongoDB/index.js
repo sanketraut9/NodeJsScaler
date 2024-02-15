ca79453e209b14b7e723fa402226aae4e6de27c4
“Sanket <sanket.raut0912@gmail.com>
2024-02-15 18:22:55 +0530

    Comparision operator, Logical operatore

--- Express/MongoDB/index.js
+++ Express/MongoDB/index.js
@@ -9,7 +9,8 @@ const courseSchema = new mongoose.Schema({
     name: String,
     creator: String,
     publishDate: {type:Date, default:Date.now},
-    isPublished: Boolean
+    isPublished: Boolean,
+    rating:Number
 })
 
 
@@ -19,9 +20,10 @@ const Course = mongoose.model('couse', courseSchema)
 
 async function createCourse(){
     const course = new Course({
-        name:'JavaScript',
-        creator:'Sanket',
-        isPublished: true
+        name:'python',
+        creator:'Ron',
+        isPublished: true,
+        rating: 2.2
     })
     
     const result = await course.save()
@@ -29,8 +31,15 @@ async function createCourse(){
 }
 
 
+//Comparision operator::
+// eq(equal) // gt(greater than) // gte(greater than equal)
+// lt(less than)  // lte(less than equal)  // in   //not in
+
+// Logical operatore::
+// or     //and
+
 async function getCourses(){
-    const courses  = await Course.find({creator:'Sanket'}).select({name:1, publishDate:1}).sort({name:1})
+    const courses  = await Course.find({rating : {$in : [2.2, 3.2]}}).select({name:1, publishDate:1}).or([{creator:'Dyana'}, {rating: 3.2}])
     console.log(courses);
 }
 
