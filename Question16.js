9b4ca93091e1512bdcc34eab01356a26f36f45fb
“Sanket <sanket.raut0912@gmail.com>
2024-02-17 02:00:59 +0530

    Question16

---
+++ Question16.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose')
+
+const app = express();
+const port = 3000;
+
+
+const mongoDBUrl = 'mongodb://127.0.0.1/testdata';
+
+// Function to establish connection to MongoDB
+function connectToMongoDB() {    
+    try {
+        mongoose.connect(mongoDBUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log('Connected to mongoDb');
+    } catch (err) {
+        console.error('Error connecting to MongoDB:', err);
+    }
+}
+
+connectToMongoDB();
+
+app.get('/', (req, res) => {
+    res.send('Express application with MongoDB integration');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
